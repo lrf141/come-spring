be03ff62d36d485c316efa55b6eeffe28d8446e9
lrf141 <lucky.rabbitfoot141@gmail.com>
2020-07-04 21:23:18 +0900

    add sample spring boot application

--- src/main/java/come/spring/App.java
+++ src/main/java/come/spring/App.java
@@ -3,12 +3,29 @@
  */
 package come.spring;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
+    public static void main(String... args) {
+        SpringApplication.run(App.class, args);
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            System.out.println("Let's inspect the bean provided by Spring Boot: ");
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+        };
     }
 }
---
+++ src/main/java/come/spring/HelloController.java
@@ -0,0 +1,12 @@
+package come.spring;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+    @RequestMapping
+    public String index() {
+        return "Greeting from Spring Boot!";
+    }
+}
