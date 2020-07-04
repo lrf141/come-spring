848e87df1c8d860078fa9ef4b305ccff37229645
lrf141 <lucky.rabbitfoot141@gmail.com>
2020-07-04 23:05:38 +0900

    remove cli runner

--- src/main/java/come/spring/App.java
+++ src/main/java/come/spring/App.java
@@ -16,16 +16,4 @@ public class App {
     public static void main(String... args) {
         SpringApplication.run(App.class, args);
     }
-
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-            System.out.println("Let's inspect the bean provided by Spring Boot: ");
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-        };
-    }
 }
